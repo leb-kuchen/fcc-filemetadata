ca62b598e284e317993bb1cf52b257ca60457a5b
Dominic Gerhauser <dominicgerhauser1@gmail.com>
2023-09-01 18:11:01 +0200

    easy

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 var express = require('express');
 var cors = require('cors');
+const bodyParser = require('body-parser')
 require('dotenv').config()
-
+const multer = require("multer")
+const upload = multer({dest: "uploads"})
 var app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}))
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  let {originalname: name, mimetype: type, size} = req.file
+  console.log(req.file)
+  res.json({name, type, size})
+})
 
 
 
